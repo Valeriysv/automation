d0f866b5e2ca96ae71c05d984d4f4e14e89451cd
Valeriysv <pbsvetlov1989@gmail.com>
2023-03-01 17:23:17 +0200

    Automation
    Automation

---
+++ src/test/java/Authorization.java
@@ -0,0 +1,44 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Authorization {
+    WebDriver driver = new ChromeDriver();
+    Data getDate = new Data();
+
+    private By btnLogin = By.linkText("Log in");
+    private By inputEmail = By.xpath("//div[@class='form-group']//input[@id='user_email']");
+    private By inputPassword = By.xpath("//div[@class='form-group']//input[@id='user_password']");
+    private By btmJoin = By.name("commit");
+    private By getErrorText = By.xpath("//div[@class='row']//div[@class='col-xs-10 col-sm-6 center-block flash__message']");
+
+
+    @Test
+    public void authorization(){
+        driver.get(getDate.getUrl);
+        driver.manage().window().maximize();
+        driver.findElement(btnLogin).click();
+        driver.findElement(inputEmail).sendKeys(getDate.email);
+        driver.findElement(inputPassword).sendKeys(getDate.password);
+        driver.findElement(btmJoin).click();
+        
+
+    }
+    @Test
+    public void negativeAuthorization(){
+        driver.get(getDate.getUrl);
+        driver.manage().window().maximize();
+        driver.findElement(btnLogin).click();
+        driver.findElement(inputEmail).sendKeys(getDate.email);
+        driver.findElement(inputPassword).sendKeys(getDate.password);
+        driver.findElement(btmJoin).click();
+        String getError = driver.findElement(getErrorText).getText();
+
+        Assert.assertEquals("Invalid email or password.", getError);
+
+    }
+
+
+}
---
+++ src/test/java/Data.java
@@ -0,0 +1,9 @@
+public class Data {
+    public String getUrl = "https://unsplash.com/";
+    public String firstName = "Valerifsdy123456";
+    public String lastName = "Shevchenko123455";
+    public String email = "newShevchenko1234gdfdf55@gmail.com";
+    public String userName = "Shevchenkodf12gd3455";
+    public String password = "12345678";
+
+}
